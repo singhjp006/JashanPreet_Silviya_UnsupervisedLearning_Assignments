b80b3d9e24dab553341a3a6a39039d90b64b3321
Jashan Preet Singh <jashanpreet@khalsa.com>
2021-11-21 23:32:50 -0500

    Update JashanPreet_Silviya_Assignment4_Group_6.py

--- JashanPreet_Silviya_Assignment4_Group6/JashanPreet_Silviya_Assignment4_Group_6.py
+++ JashanPreet_Silviya_Assignment4_Group6/JashanPreet_Silviya_Assignment4_Group_6.py
@@ -103,33 +103,6 @@ for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_, color_i
     ell.set_clip_box(splot.bbox)
     ell.set_alpha(0.5)
     splot.add_artist(ell)
-
-plt.xticks(())
-plt.yticks(())
-plt.title(
-    f"Selected GMM: {best_gmm.covariance_type} model, "
-    f"{best_gmm.n_components} components"
-)
-plt.subplots_adjust(hspace=0.35, bottom=0.02)
-plt.show()
-
-# Plot the winner
-splot = plt.subplot(2, 1, 2)
-Y_ = clf.predict(pca_X_train)
-for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_, color_iter)):
-    v, w = linalg.eigh(cov)
-    if not np.any(Y_ == i):
-        continue
-    plt.scatter(pca_X_train[Y_ == i, 0], pca_X_train[Y_ == i, 1], 0.8, color=color)
-
-    # Plot an ellipse to show the Gaussian component
-    angle = np.arctan2(w[0][1], w[0][0])
-    angle = 180.0 * angle / np.pi  # convert to degrees
-    v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
-    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180.0 + angle, color=color)
-    ell.set_clip_box(splot.bbox)
-    ell.set_alpha(0.5)
-    splot.add_artist(ell)
     
     
 '''4. Plot the results from (2) and (3). '''
